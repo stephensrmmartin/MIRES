be29e22cec8ddf2078e46257b0f6a117c22ec126
Stephen Martin <stephensrmmartin@gmail.com>
2020-12-30 19:25:03 -0800

    Test file check in.

--- test/dpHmre.R
+++ test/dpHmre.R
@@ -18,7 +18,7 @@ dpHn <- stan_model("test/dpHNormal.stan")
 # Estimate [sampling, vb, or optimizing]
 K <- 50
 stan_data <- list(N = length(dpTest), y = dpTest, K = K)
-dpOut_sample <- sampling(dpHn, data = stan_data, iter = 500, cores = 4)
+## dpOut_sample <- sampling(dpHn, data = stan_data, iter = 500, cores = 4)
 dpOut <- vb(dpHn, data = stan_data,
             algorithm = "meanfield",
             importance_resampling = TRUE, tol_rel_obj = .005)
@@ -29,8 +29,8 @@ dpOut2 <- Fit(DirichletProcessExponential(dpTest), 500)
 
 # Plot hist, true density, stan density, and dirichletprocess density
 hist(dpTest, breaks = 300, probability = TRUE)
-curve(dhmre(x,-1, 1), 0, 4, n = 1000, add = TRUE)
-curve(predictMixture(x, dpOut, K, dens = dposNorm, params = c("location", "scale"), R_params = c("mu", "sd"))[, "mean"], 0, 4, n = 1000, add = TRUE, col = "red")
+curve(dhmre(x,-1, 1), 0, 4, n = 1000, add = TRUE, lwd = 3)
+curve(predictMixture(x, dpOut, K, dens = dposNorm, params = c("location", "scale"), R_params = c("mu", "sd"))[, "mean"], 0, 4, n = 1000, add = TRUE, col = "red", lwd = 3)
 ## curve(predictMixture(x, dpOut, K, dens = dexp, params = c("rate"), R_params = c("rate"))[,"mean"], 0, 4, n = 1000, add = TRUE, col = "red")
-curve(dlogspline(x,logspline(dpTest, lbound = 0)), 0, 5, n = 1000, add = TRUE, col = "purple")
+curve(dlogspline(x,logspline(dpTest, lbound = 0)), 0, 5, n = 1000, add = TRUE, col = "blue")
 curve(PosteriorFunction(dpOut2)(x), 0, 4, n = 1000, add = TRUE, col = "green")
