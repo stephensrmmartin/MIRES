27121dcb82066a1ce954ba5418a9da15ef6bc5a1
Stephen Martin <stephensrmmartin@gmail.com>
2021-01-04 11:46:48 -0800

    Test changes.

--- test/dpHmre.R
+++ test/dpHmre.R
@@ -8,18 +8,18 @@ dposNorm <- function(x, mu, sd) {
 
 # Generate data
 set.seed(13)
-dpTest <- MIRES:::rhmre(4000, -1, 1)
+dpTest <- MIRES:::rhmre(4000, -2, .5)
 
 # Get Stan method
 dfun.stan <- MIRES:::.density.stan(dpTest, K = 200)
 
 # Compare
-MIRES:::dhmre(0, -1, 1)
+MIRES:::dhmre(0, -2, .5)
 dfun.stan(0)
 logspline::dlogspline(0, logspline::logspline(dpTest, lbound = 0))
 
 # Plot hist, true density, stan density, and dirichletprocess density
 hist(dpTest, breaks = 300, probability = TRUE)
-curve(MIRES:::dhmre(x,-1, 1), 0, 4, n = 1000, add = TRUE, lwd = 3)
-curve(dfun.stan(x), 0, 4, n = 1000, add = TRUE, col = "red", lwd = 3)
-curve(dlogspline(x,logspline(dpTest, lbound = 0)), 0, 5, n = 1000, add = TRUE, col = "blue")
+curve(MIRES:::dhmre(x,-2, .5), 0, .01, n = 1000, add = TRUE, lwd = 3)
+curve(dfun.stan(x), 0, .01, n = 1000, add = TRUE, col = "red", lwd = 3)
+curve(dlogspline(x,logspline(dpTest, lbound = 0)), 0, .01, n = 1000, add = TRUE, col = "blue")
