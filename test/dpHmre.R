3a86435ffc9bd12569374f8beffcdde006307f7b
Stephen R. Martin <stephenSRMMartin@gmail.com>
2021-01-04 17:46:34 -0800

    Simplified test params

--- test/dpHmre.R
+++ test/dpHmre.R
@@ -8,7 +8,9 @@ dposNorm <- function(x, mu, sd) {
 
 # Generate data
 set.seed(13)
-dpTest <- MIRES:::rhmre(4000, -2, .5)
+hmre_mu <- -.2
+hmre_sigma <- .5
+dpTest <- MIRES:::rhmre(4000, hmre_mu, hmre_sigma)
 
 # Get Stan method
 dfun.stan <- MIRES:::.density.stan(dpTest, K = 100)
@@ -19,13 +21,13 @@ stan_data <- list(N = length(dpTest), y = dpTest, K = 100)
 spikedOut <- vb(spiked, stan_data, tol_rel_obj = .005)
 
 # Compare
-MIRES:::dhmre(0, -2, .5)
+MIRES:::dhmre(0, hmre_mu, hmre_sigma)
 dfun.stan(0)
 summary(spikedOut, pars = c("py_0", "pi_mix"))$summary
 logspline::dlogspline(0, logspline::logspline(dpTest, lbound = 0))
 
 # Plot hist, true density, stan density, and dirichletprocess density
 hist(dpTest, breaks = 300, probability = TRUE)
-curve(MIRES:::dhmre(x,-2, .5), 0, .01, n = 1000, add = TRUE, lwd = 3)
+curve(MIRES:::dhmre(x,hmre_mu, hmre_sigma), 0, .01, n = 1000, add = TRUE, lwd = 3)
 curve(dfun.stan(x), 0, .01, n = 1000, add = TRUE, col = "red", lwd = 3)
 curve(dlogspline(x,logspline(dpTest, lbound = 0)), 0, .01, n = 1000, add = TRUE, col = "blue")
