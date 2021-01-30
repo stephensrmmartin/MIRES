1eb5d72a3d67954349568393fd93491f1191a6d1
Stephen Martin <stephensrmmartin@gmail.com>
2021-01-30 00:04:28 -0800

    Test fns.

--- test/unidim.R
+++ test/unidim.R
@@ -13,26 +13,44 @@ fixed <- list(lambda = rep(.7, J), resid_log = rep(log(sqrt(1 - .7^2)), J), nu =
 mipattern <- list("items", .4, floor(J/2))
 etadist <- "std"
 
-mipattern <- list("none")
+## mipattern <- list("none")
 
-## d <- readRDS("~/Output/MIRES/fit_gen.Rds")
 d <- MIRES:::datagen_uni(J, K, n, fixed, mipattern, etadist = etadist)
-## saveRDS(d, "~/Output/MIRES/fit_gen.Rds")
 
 ds <- d$df
 
-## fit <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25)
-fit <- readRDS("~/Output/MIRES/fit.Rds")
-## fit_hier <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, sum_coding = FALSE)
-## saveRDS(fit_hier, "~/Output/MIRES/fit_hier.Rds")
-fit_hier <- readRDS("~/Output/MIRES/fit_hier.Rds")
+# HMRE
+fit <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25)
 
-## fit_hier_re <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, sum_coding = FALSE, hmre = FALSE)
-## saveRDS(fit_hier_re, "~/Output/MIRES/fit_hier_re.Rds")
-fit_hier_re <- readRDS("~/Output/MIRES/fit_hier_re.Rds")
+# Hierarchical HMRE
+fit_hier <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, sum_coding = FALSE)
 
-# Testing marginalized version
+# Hierarchical non-HMRE
+fit_hier_re <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, sum_coding = FALSE, hmre = FALSE)
+
+# Hierarchical Marg.
 fit_hier_marg <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, sum_coding = FALSE, marginalize=TRUE)
+
+##################
+# COMBINED MODEL #
+##################
+
+# HMRE (Combined)
+fit <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, combined = TRUE)
+
+# Hierarchical HMRE (Combined)
+fit_hier <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, sum_coding = FALSE, combined = TRUE)
+
+# Hierarchical non-HMRE (Combined)
+fit_hier_re <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, sum_coding = FALSE, hmre = FALSE, combined = TRUE)
+
+# Hierarchical Marg. (Combined)
+fit_hier_marg <- mires(myfactor ~ x_1 + x_2 + x_3 + x_4 + x_5 + x_6 + x_7 + x_8 + x_9 + x_10, group = group, ds, iter = 1000, prior_only = FALSE, hmre_mu = 0, hmre_scale = .25, sum_coding = FALSE, marginalize=TRUE, combined = TRUE)
+
+
+########
+# Misc #
+########
 
 # Get mean times (in minutes)
 mean(rowMeans(rstan::get_elapsed_time(fit_hier_marg$fit))) / 60
