1608137d7f02767e660cdf7aeec9c3a62a21cf19
Stephen Martin <stephensrmmartin@gmail.com>
2021-02-12 15:19:51 -0800

    Added simulated data files.

---
+++ R/data.R
@@ -0,0 +1,34 @@
+#' Simulated data: No variance (Full invariance)
+#'
+#' @format A simulated dataset containing eight indicators for one factor, and one grouping variable.
+"sim_none"
+
+
+#' Simulated data: All parameters vary (Full non-invariance)
+#'
+#' @format A simulated dataset containing eight indicators for one factor, and one grouping variable.
+"sim_all"
+
+#' Simulated data: Half the items are non-invariant.
+#'
+#' @format A simulated dataset containing eight indicators for one factor, and one grouping variable.
+"sim_items"
+
+
+#' Simulated data: Intercepts vary
+#'
+#' @format A simulated dataset containing eight indicators for one factor, and one grouping variable.
+"sim_intercepts"
+
+
+#' Simulated data: Loadings vary
+#'
+#' @format A simulated dataset containing eight indicators for one factor, and one grouping variable.
+"sim_loadings"
+
+
+#' Simulated data: Residual variances vary
+#'
+#' @format A simulated dataset containing eight indicators for one factor, and one grouping variable.
+"sim_resid"
+
--- data-raw/datagen.R
+++ data-raw/datagen.R
@@ -6,15 +6,28 @@ K <- 5
 n <- 50
 
 fixed <- list(lambda = rep(.7, J), resid_log = rep(log(sqrt(1 - .7^2)), J), nu = rep(0, J))
-etadist <- "std"
+etadist <- NULL
 
 mipatterns <- list(
     none = list("none"),
-    constant = list("constant", .4),
+    all = list("constant", .4),
     items = list("items", .4, J/2),
     loadings = list("params", .4, 0),
     resid = list("params", .4, 1),
-    nu = list("params", .4, 2)
+    intercepts = list("params", .4, 2)
 )
 
 datasets <- lapply(mipatterns, MIRES:::datagen_uni, J = J, K = K, n = n, etadist = etadist, fixed = fixed)
+
+names(datasets) <- paste0("sim_", names(datasets))
+
+for(d in names(datasets)) {
+    assign(d, datasets[[d]]$df)
+}
+
+usethis::use_data(sim_none)
+usethis::use_data(sim_all)
+usethis::use_data(sim_items)
+usethis::use_data(sim_intercepts)
+usethis::use_data(sim_loadings)
+usethis::use_data(sim_resid)
